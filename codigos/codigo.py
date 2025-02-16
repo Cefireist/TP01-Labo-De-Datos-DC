795031e68a511408d2f1196cac77c08821df7cce
Sebastian Ceffalotti <sebastian.ceffalotti@gmail.com>
2025-02-16 18:22:48 -0300

    Agregadas las tablas faltantes, si no hay errores de datos faltantes deberian las tablas finales estar en tercera forma normal y con los datos necesarios para hacer las consignas del TP, asi que habria que revisar

--- codigos/codigo.py
+++ codigos/codigo.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-OBSERVACION IMPORTANTE:
+OBSERVACIONES IMPORTANTES:
     la base de datos con mas departamentos es la de establecimientos educativos, que a
     diferencia del padron de personas, incluye el departamento de ANTARTIDA en Tierra Del 
     Fuego. Como no tenemos datos de la poblacion alli ya que no figuran en el padron de personas,
-    lo sacamos de las tablas, ya que si no reportariamos nan por ejemplo en el primer inciso
+    lo sacamos de las tablas, ya que si no reportariamos nan por ejemplo en el primer inciso.
+    Tambien en la tabla de centros culturales figura como departamento ciudad autonoma de buenos
+    aires en lugar de por cada comuna como en las tablas de padron de personas y de establecimientos
+    educativos, asi que agregue a "departamentos" una tupla que sea como la inclusion de todo, 
+    asi esta con id_prov = 2 e id_depto = 0 (como figura en centros culturales).
+    Las tablas finales a usar serian centros_culturales, departamento, grupo_etario, provincia
+    y establecimientos educativos.
 """
 
 # %% Importación de librerías
@@ -73,7 +79,7 @@ pp = dd.sql(
     ).df()
 
 # Finalmente solo me quedo con id_provincia, id_depto, edad y numero de casos
-pp = dd.sql(
+grupo_etario_personas = dd.sql(
     """ 
     SELECT CAST(SUBSTRING(codigo_area, 1, 3) AS INTEGER) AS id_provincia,
     CAST(SUBSTRING(codigo_area, 4, 3) AS INTEGER) AS id_departamento, Edad, Casos
@@ -81,6 +87,32 @@ pp = dd.sql(
     """
     ).df()
 
+#%% TABLA departamento, tiene el nombre de departamento, el id_departamento, el id_provincia
+# le agrego como caso particular el departamento "Ciudad Autonoma De Buenos Aires" de id_depto = 0
+# y id_prov = 2 ya que en centros culturales no hay info sobre cada comuna
+
+departamento = dd.sql(
+    """ 
+    SELECT DISTINCT CAST(SUBSTRING(codigo_area, 1, 3) AS INTEGER) AS id_provincia,
+    CAST(SUBSTRING(codigo_area, 4, 3) AS INTEGER) AS id_departamento,
+    UPPER(nombre_depto) AS nombre_depto
+    FROM pp
+    UNION 
+    SELECT DISTINCT ID_PROV, CAST(SUBSTRING(CAST(ID_DEPTO AS VARCHAR), 2, 4) AS INTEGER) AS id_depto, Departamento
+    FROM cc
+    WHERE ID_PROV = 2
+    """
+    ).df()
+
+#%%
+centros_culturales = dd.sql(
+    """
+    SELECT DISTINCT id_cc, ID_PROV AS id_prov, 
+    CAST(RIGHT(CAST(id_depto AS VARCHAR), LENGTH(CAST(id_depto AS VARCHAR)) - LENGTH(CAST(id_prov AS VARCHAR))) AS INTEGER) AS id_depto,
+    Capacidad, "Mail " AS Mail
+    FROM cc;
+    """).df()
+    
 #%% TABLA provincia
 
 provincia = dd.sql(
@@ -90,24 +122,14 @@ provincia = dd.sql(
     ORDER BY id_provincia;
     """
     ).df()
-#%% TABLA departamento, tiene el nombre de departamento, el id_departamento, el id_provincia
 
-depto = dd.sql(
-    """ 
-    SELECT DISTINCT CAST(SUBSTRING(codigo_area, 1, 3) AS INTEGER) AS id_provincia,
-    CAST(SUBSTRING(codigo_area, 4, 3) AS INTEGER) AS id_departamento,
-    UPPER(nombre_depto) AS nombre_depto
-    FROM pp
-    """
-    ).df()
-
-#%% Busco formar una tabla llamada cod_ee con cueanexo, id_depto e id_provincia
+#%% Busco formar una tabla llamada cod_ee con cueanexo, id_depto e id_provincia, etc.
 
 # paso a mayusculas todo y cambio los datos de ciudad de buenos aire y tierra del fuego para que 
 # haga match con los de la tabla provincia. en esta tabla la clave primaria va a ser cueanexo
 _cod_ee_normalizado = dd.sql(
     """ 
-    SELECT Cueanexo, UPPER(Departamento) AS Departamento,
+    SELECT UPPER(Departamento) AS Departamento, *,
     CASE WHEN Jurisdicción = 'Ciudad de Buenos Aires' THEN 'CIUDAD AUTÓNOMA DE BUENOS AIRES'
     WHEN Jurisdicción = 'Tierra Del Fuego' THEN 'TIERRA DEL FUEGO, ANTÁRTIDA E ISLAS DEL ATLÁNTICO SUR'
     ELSE UPPER(Jurisdicción)
@@ -118,29 +140,24 @@ _cod_ee_normalizado = dd.sql(
 
 # Hago un inner join de esta tabla normalizada con provincia para obtener id_provincia, y luego
 # otro inner join para juntar depto (uso nombre departamento e id provincia como claves)
-cod_ee = dd.sql(
+establecimientos_educativos = dd.sql(
     """
-    SELECT n.Cueanexo, p.id_provincia, d.id_departamento
+    SELECT n.Cueanexo, p.id_provincia, d.id_departamento,
+    "Nivel inicial - Jardín maternal" AS jar_mat,
+    "Nivel inicial - Jardín de infantes" AS jar_inf, Primario, Secundario,
+    "Secundario - INET" AS Secundario_tecnico
     FROM _cod_ee_normalizado AS n 
     INNER JOIN provincia AS p
     ON n.provincia = p.nombre_provincia
-    INNER JOIN depto AS d
+    INNER JOIN departamento AS d
     ON p.id_provincia = d.id_provincia AND n.Departamento = d.nombre_depto
     """
     ).df()
 
-#%%
-
-ee_modalidad_comun = dd.sql(
-    """
-    SELECT Cueanexo, "Nivel inicial - Jardín maternal" AS jar_mat,
-    "Nivel inicial - Jardín de infantes" AS jar_inf, Primario, Secundario,
-    "Secundario - INET" AS Secundario_tecnico
-    FROM ee
-    """
-    ).df()
 
 
+
+    
 #%%
 
 
