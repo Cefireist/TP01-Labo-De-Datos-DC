65250b3edeb4510b05efc67b5d01c19c780b6314
Aaron Cuellar <aaroncuellar2003@gmail.com>
2025-02-17 01:47:35 -0300

    renombre variables y comentarios haciendolos mas representativos. Ademas corregi query grupo_etario_personas y departamentos

--- Codigo/main.py
+++ Codigo/main.py
@@ -19,38 +19,39 @@ import duckdb as dd
 import pandas as pd 
 import os
 
-#%% Obtengo las rutas de los archivos
-# obtengo la ruta del directorio donde se esta ejecutando este codigo
-_ruta_actual = os.path.dirname(__file__)
+#%% Obtencion rutas de los archivos
 
-# construyo la ruta a la carpeta hermana datos-puros que es donde estan los archivos
-_ruta_carpeta = os.path.join(_ruta_actual, './TablasOriginales')
+# ruta del directorio donde se esta ejecutando este codigo
+ruta_actual = os.path.dirname(__file__)
+# ruta a la carpeta TablasOriginales que es donde estan las tablas
+ruta_tablas_originales = os.path.join(ruta_actual, 'TablasOriginales')
 
-# Armo las rutas para los 3 archivos, los establecimientos educativos y el padron de personas
-# son .xlSx, no .csv
-_ruta_cc = os.path.join(_ruta_carpeta, 'centros_culturales_2022.csv')
-_ruta_ee = os.path.join(_ruta_carpeta, 'padron_establecimientos_educativos_2022.xlsx')
-_ruta_pp = os.path.join(_ruta_carpeta, 'padron_poblacion_2022.xlsX')
-# %% Lectura de tablas de centros culturales y establecimientos educativos
+# rutas de los 3 archivos
+ruta_ee = os.path.join(ruta_tablas_originales, 'padron_establecimientos_educativos_2022.xlsx')
+ruta_cc = os.path.join(ruta_tablas_originales, 'centros_culturales_2022.csv')
+ruta_pp = os.path.join(ruta_tablas_originales, 'padron_poblacion_2022.xlsX')
 
-_cc = pd.read_csv(_ruta_cc)
-# Le agrego una clave primaria, no encontre combinacion de atributos suficientemente pequena
-# como para que sea la clave primaria asi que le agregue un indice
-cc = dd.sql("""
+# %% Lectura padron establecimientos educativos y centros culturales
+establecimientos_educativos = pd.read_excel(ruta_ee, header=0, skiprows=6)
+centros_culturales = pd.read_csv(ruta_cc)
+
+# Le agrego una clave primaria a centros_culturales, 
+# no encontre combinacion de atributos suficientemente pequena como para que sea la clave primaria asi que le agregue un indice
+centros_culturales = dd.sql("""
     SELECT ROW_NUMBER() OVER () AS id_cc, *
-    FROM _cc;
+    FROM centros_culturales;
 """).df()
 
-ee = pd.read_excel(_ruta_ee, header=0, skiprows=6)
 
-#%% Lectura del padron de poblacion, transformo los datos
+#%% Lectura y limpieza del padron de poblacion
 
-# leo el archivo omitiendo las primeras 13 filas
-pp = pd.read_excel(_ruta_pp, usecols=[1,2,3,4], header=None, skiprows=13)
-pp.columns = ["Edad", "Casos", "Porcentaje", "Porcentaje_acumulado"]
+# leo el archivo omitiendo las primeras 13 filas y seleccionando columnas a usar
+poblacion = pd.read_excel(ruta_pp, usecols=[1,2,3,4], header=None, skiprows=13)
+poblacion.columns = ["Edad", "Casos", "Porcentaje", "Porcentaje_acumulado"]
 
-# HAgo como una especie de mascara, similar a pandas + numpy, luego uso ffill para rellenar los valores
-pp = dd.sql(
+## Limpieza tabla poblacion
+# se agregan columnas codigo_area y nombre_depto
+poblacion = dd.sql(
     """
     SELECT *,
     CASE WHEN Edad LIKE 'AREA%'
@@ -61,80 +62,83 @@ pp = dd.sql(
         THEN Casos
         ELSE NULL
         END AS nombre_depto
-    FROM pp
-    WHERE Edad != 'nan' AND Edad != 'Total' AND Edad != 'Edad' AND Edad != 'RESUMEN'
+    FROM poblacion
+    WHERE LOWER(Edad) NOT IN ('nan', 'total', 'edad', 'resumen')
     """).df()
 
-pp[["codigo_area", "nombre_depto"]] = pp[["codigo_area", "nombre_depto"]].ffill()
+# relleno hacia abajo con el ultimo valor no nulo
+poblacion[["codigo_area", "nombre_depto"]] = poblacion[["codigo_area", "nombre_depto"]].ffill()
 
-# Termino de filtrar, saco las filas sin info y dejo solo la parte numerica del codigo depto
-# el CAST sirve para hacer que la variable sea INTEGER (si no queda como string)
-pp = dd.sql(
+# eliminacion de filas sin info ; formateo codigo_area dejando solo el valor numerico
+poblacion = dd.sql(
     """
     SELECT Edad, Casos, Porcentaje, Porcentaje_acumulado, 
-    REPLACE(codigo_area, 'AREA #', '') AS codigo_area, nombre_depto
-    FROM pp
+    REPLACE(codigo_area, 'AREA # ', '') AS codigo_area, nombre_depto
+    FROM poblacion
     WHERE Edad NOT LIKE 'AREA%' 
     """
     ).df()
 
-# Finalmente solo me quedo con id_provincia, id_depto, edad y numero de casos
+#%% Tabla "grupo_etario_personas": guarda la edad, el número de casos, id_provincia e id_depto
+# el CAST sirve para hacer que la variable sea INTEGER (si no queda como string)
 grupo_etario_personas = dd.sql(
     """ 
-    SELECT CAST(SUBSTRING(codigo_area, 1, 3) AS INTEGER) AS id_provincia,
-    CAST(SUBSTRING(codigo_area, 4, 3) AS INTEGER) AS id_departamento, Edad, Casos
-    FROM pp
+    SELECT CAST(SUBSTRING(codigo_area, 1, 2) AS INTEGER) AS id_provincia,
+    CAST(SUBSTRING(codigo_area, 3, 3) AS INTEGER) AS id_departamento, Edad, Casos
+    FROM poblacion
     """
     ).df()
 
-#%% TABLA departamento, tiene el nombre de departamento, el id_departamento, el id_provincia
+#%% Tabla "departamentos": guarda el nombre, id_departamento e id_provincia
+
 # le agrego como caso particular el departamento "Ciudad Autonoma De Buenos Aires" de id_depto = 0
 # y id_prov = 2 ya que en centros culturales no hay info sobre cada comuna
-
-departamento = dd.sql(
+departamentos = dd.sql(
     """ 
-    SELECT DISTINCT CAST(SUBSTRING(codigo_area, 1, 3) AS INTEGER) AS id_provincia,
-    CAST(SUBSTRING(codigo_area, 4, 3) AS INTEGER) AS id_departamento,
+    SELECT DISTINCT CAST(SUBSTRING(codigo_area, 1, 2) AS INTEGER) AS id_provincia,
+    CAST(SUBSTRING(codigo_area, 3, 3) AS INTEGER) AS id_departamento,
     UPPER(nombre_depto) AS nombre_depto
-    FROM pp
+    FROM poblacion
     UNION 
     SELECT DISTINCT ID_PROV, CAST(SUBSTRING(CAST(ID_DEPTO AS VARCHAR), 2, 4) AS INTEGER) AS id_depto, Departamento
-    FROM cc
+    FROM centros_culturales
     WHERE ID_PROV = 2
     """
     ).df()
 
-#%%
+#%% TABLA "provincias": almacena nombre y identificador de cada provincia
+
+provincias = dd.sql(
+    """
+    SELECT DISTINCT ID_PROV AS id_provincia, UPPER(provincia) AS nombre_provincia 
+    FROM centros_culturales
+    ORDER BY id_provincia;
+    """
+    ).df()
+
+#%% recorte de info extraida de centros_culturales
+
+# extraigo id_cc id_depto id_prov capacidad mail
 centros_culturales = dd.sql(
     """
     SELECT DISTINCT id_cc, ID_PROV AS id_prov, 
     CAST(RIGHT(CAST(id_depto AS VARCHAR), LENGTH(CAST(id_depto AS VARCHAR)) - LENGTH(CAST(id_prov AS VARCHAR))) AS INTEGER) AS id_depto,
     Capacidad, "Mail " AS Mail
-    FROM cc;
+    FROM centros_culturales;
     """).df()
-    
-#%% TABLA provincia
 
-provincia = dd.sql(
-    """
-    SELECT DISTINCT ID_PROV AS id_provincia, UPPER(provincia) AS nombre_provincia 
-    FROM cc
-    ORDER BY id_provincia;
-    """
-    ).df()
+#%% Tabla "cod_ee_normalizado": con cueanexo, id_depto e id_provincia, etc.
 
-#%% Busco formar una tabla llamada cod_ee con cueanexo, id_depto e id_provincia, etc.
-
-# paso a mayusculas todo y cambio los datos de ciudad de buenos aire y tierra del fuego para que 
+# paso a mayusculas todo y cambio los datos de ciudad de buenos aires y tierra del fuego para que 
 # haga match con los de la tabla provincia. en esta tabla la clave primaria va a ser cueanexo
-_cod_ee_normalizado = dd.sql(
+cod_ee_normalizado = dd.sql(
     """ 
     SELECT UPPER(Departamento) AS Departamento, *,
     CASE WHEN Jurisdicción = 'Ciudad de Buenos Aires' THEN 'CIUDAD AUTÓNOMA DE BUENOS AIRES'
     WHEN Jurisdicción = 'Tierra Del Fuego' THEN 'TIERRA DEL FUEGO, ANTÁRTIDA E ISLAS DEL ATLÁNTICO SUR'
     ELSE UPPER(Jurisdicción)
     END AS provincia,
-    FROM ee
+    FROM establecimientos_educativos
     """
     ).df()
 
@@ -146,19 +150,13 @@ establecimientos_educativos = dd.sql(
     "Nivel inicial - Jardín maternal" AS jar_mat,
     "Nivel inicial - Jardín de infantes" AS jar_inf, Primario, Secundario,
     "Secundario - INET" AS Secundario_tecnico
-    FROM _cod_ee_normalizado AS n 
-    INNER JOIN provincia AS p
+    FROM cod_ee_normalizado AS n 
+    INNER JOIN provincias AS p
     ON n.provincia = p.nombre_provincia
-    INNER JOIN departamento AS d
+    INNER JOIN departamentos AS d
     ON p.id_provincia = d.id_provincia AND n.Departamento = d.nombre_depto
     """
     ).df()
 
-
-
-
-    
 #%%
 
-
-#%%
